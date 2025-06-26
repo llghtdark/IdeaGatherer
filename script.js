fd39ed0f06a9dd76c3074a9ce5164886aaf4a840
llghtdark <lcfilho19@gmail.com>
2025-06-26 12:38:31 -0300

    fixed some stuf

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// // Function to save the text to local storage as a text file
-// document.getElementById('saveButton').addEventListener('click', function() {
-//     var textToSave = document.getElementById('ideaScroll').value;
-//     var blob = new Blob([textToSave], { type: 'text/plain' });
-//     var url = URL.createObjectURL(blob);
-//     localStorage.setItem('savedTextFile', url);
-// });
-
-// // Function to load the text from local storage and display it
-// document.getElementById('loadButton').addEventListener('click', function() {
-//     var savedUrl = localStorage.getItem('savedTextFile');
-//     if (savedUrl) {
-//         fetch(savedUrl)
-//             .then(response => response.text())
-//             .then(text => {
-//                 document.getElementById('ideaScroll').value = text;
-//             })
-//             .catch(error => {
-//                 console.error('Error loading text file:', error);
-//             });
-//     } else {
-//         console.error('No text file found in local storage.');
-//     }
-// });
-
 const tada = new Audio("tada.wav");
 
 let ideaList = localStorage.getItem('List');
@@ -42,19 +17,33 @@ document.getElementById('saveButton').addEventListener('click', function() {
     }
 });
 
+
+let number;
+let lastnumber = number;
 document.getElementById('loadButton').addEventListener('click', function() {
-    let number = getRndInteger();
+    
     console.log(number);
+    console.log(lastnumber);
+
+    
+    do {
+        number = getRndInteger();
+    } while (Ideas.length > 1 && number === lastnumber);
+    
     if (Ideas[0] === undefined) {
         alert("bitch it ain nothin to show");
-    } else {
+        return;
+        } else {
         Idea.innerHTML = Ideas[number];
-    }
+        lastnumber = number;
+    } 
+
     
 });
 document.getElementById('clean').addEventListener('click', function() {
     localStorage.setItem('List', JSON.stringify([]));
     Ideas.length = 0;
+    Idea.innerHTML = "";
 });
 
 function getRndInteger() {
